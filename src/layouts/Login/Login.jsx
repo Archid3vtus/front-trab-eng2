acc98892ec3c4d72163ce9c7c2979c093a53f2fd
Yuri Braga <yuri@Yuri-PC.localdomain>
2019-10-08 10:34:36 -0300

    Login form

--- src/layouts/Login/Login.jsx
+++ src/layouts/Login/Login.jsx
@@ -1,8 +1,55 @@
 import React, { Component } from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardText,
+  Form,
+  FormGroup,
+  Input,
+  Row,
+  Col,
+  Button
+} from "reactstrap";
+
+//css
+import "../../assets/css/login.css";
 
 class Login extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="logina">
+        <Row>
+          <Col xs="12">
+            <Card>
+              <CardHeader>
+                <h5 className="title">Login</h5>
+              </CardHeader>
+              <CardBody>
+                <Form>
+                  <Row>
+                    <Col xs="12">
+                      <FormGroup>
+                        <Input placeholder="Email" type="text" />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col xs="12">
+                      <FormGroup>
+                        <Input placeholder="Email" type="text" />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                </Form>
+              </CardBody>
+              <CardFooter></CardFooter>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    );
   }
 }
 
